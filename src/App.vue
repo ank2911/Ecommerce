01d873d97cea779c159978da815e7c688af7935f
Your Name <you@example.com>
2024-08-13 11:20:47 +0530

    Footer Section

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script>
-import Navbar from './components/Navbar.vue';
+ import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
 
 <template>
-  <Navbar />
+  
+    <Navbar /> 
+    <Footer />
+    
+
+  
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
-import { createApp } from 'vue'
-import './style.css'
+// import { createApp } from 'vue'
+// // import './style.css'
+// import 'vuetify/styles'
+// import { createVuetify } from 'vuetify'
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
+// import App from './App.vue'
+ 
+// const vuetify = createVuetify({
+//   components,
+//   directives
+// })
+// createApp(App).use(vuetify).mount('#app')
+ 
+ import { createApp } from 'vue'
+// import './style.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import App from './App.vue'
+import Footer from './components/Footer.vue' // Import the Footer component
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+const app = createApp(App)
+app.use(vuetify)
+
+// Register the Footer component globally
+app.component('Footer', Footer)
+
+app.mount('#app')
