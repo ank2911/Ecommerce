aa2a46cb6882bfffe51fab6c30c9c274f0bfac2f
Ankit Burman <AB001028592@techmahindra.com>
2024-08-16 15:09:44 +0530

    Add to Cart button updated on Search Page

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -15,23 +15,38 @@
         <p><strong>Rating:</strong> {{ items.rating }}</p>
       </v-card-text>
       <v-card-actions>
-        <v-btn @click="addProductToCart(items)">Add to Cart</v-btn>
+        <div v-if="getProductQuantity(items) > 0">
+              <v-btn icon @click="cartStore.delCart(items)">
+                <v-icon>mdi-minus</v-icon>
+              </v-btn>
+              <span>&nbsp;{{ getProductQuantity(items) }}</span>
+              <v-btn icon @click="cartStore.addCart(items)">
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </div>
+        <v-btn v-else color="primary" @click="cartStore.addCart(items)">Add to Cart</v-btn>
       </v-card-actions>
     </v-card>
   </v-container>
 </template>
 <script>
 import { useSearchStore } from "../stores/search";
+import { useCartStore } from '../stores/cartStore';
 export default {
   name: "search",
   data() {
     return {
       searchItem: useSearchStore(),
+      cartStore: useCartStore(),
     };
   },
   methods: {
     discountedPrice(price, discount) {
       return (price - (price * discount) / 100).toFixed(2);
+    },
+    getProductQuantity(product) {
+      const item = this.cartStore.cart.find((item) => item.id === product.id);
+      return item ? item.qty : 0;
     },
   },
 };
